273ff6a1672810723c22fce31b6caf948fe34030
e1turin <ivturnik2003@gmail.com>
2021-11-23 20:47:16 +0300

    main4 added new data format usage, report changed

--- lab-4/main4.py
+++ lab-4/main4.py
@@ -1,5 +1,6 @@
 import yaml
 import bson
+import toml
 
 
 def main():
@@ -8,8 +9,15 @@ def main():
 
     print("real data: ", data)
 
+    print("\n-----------\n")
+
     bsondata = bson.dumps(data)
     print("binary data: ", bsondata)
+
+    print("\n-----------\n")
+
+    tomldata = toml.dumps(data)
+    print("toml data:\n\n", tomldata.strip())
 
 
     with open('assets/timetable.bson', 'wb') as outputstream:  # You will need 'wb' mode in Python 2.x
@@ -18,7 +26,25 @@ def main():
     with open('assets/timetable.bson', "br") as inputstream:
         data = bson.loads(inputstream.readline())
 
-    print("decoded data: ", data)
+    print("\n-----------\n")
+
+    print("decoded BSON data: ", data)
+
+    with open('assets/timetable.toml', 'w', encoding="UTF-8") as outputstream:  # You will need 'wb' mode in Python 2.x
+        outputstream.write(tomldata)
+
+    with open('assets/timetable.toml', "r", encoding="UTF-8") as inputstream:
+        inputstring = ''.join(inputstream.readlines())
+
+    print("\n-----------\n")
+
+    data = toml.loads(inputstring)
+    print("decoded TOML data: ", data)
+    # # same resalt:
+    # data = toml.load('./assets/timetable.toml')
+    # print("decoded TOML data from path: ", data)
+
+
 
 
 if __name__ == "__main__":
